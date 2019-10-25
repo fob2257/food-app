bac5a3ba604ad6797581611fd47a8a13fa80b9b3
Saul Meneses <fob2257@gmail.com>
2019-10-25 13:28:40 -0700

    Axios request to yelp on search bar submit

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import yelp from '../api/yelp';
 
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
-  const onSearchSubmit = (term = '') => {
-    console.log('search term was submitted!');
+  const [results, setResults] = useState([]);
+
+  const onSearchSubmit = async (term = '') => {
+    const response = await yelp.get('/search', {
+      params: {
+        term,
+        limit: 50,
+        location: 'Hermosillo, Sonora',
+      },
+    });
+
+    const { data: { business } } = response;
+    setResults(business);
   };
 
   return (
-    <View style={styles.container}>
+    <View>
       <SearchBar onSubmit={onSearchSubmit} />
       <Text>Search Screen</Text>
+      <Text>{`Found ${results.length} results!`}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
