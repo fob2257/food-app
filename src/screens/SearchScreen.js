9d99c3f8570ded5217238d78c9f70ddc4d7bd6d1
Saul Meneses <fob2257@gmail.com>
2019-10-25 14:14:58 -0700

    Setting results when component mounts

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import yelp from '../api/yelp';
@@ -7,25 +7,39 @@ import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [results, setResults] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
 
-  const onSearchSubmit = async (term = '') => {
-    const response = await yelp.get('/search', {
-      params: {
-        term,
-        limit: 50,
-        location: 'Hermosillo, Sonora',
-      },
-    });
+  const searchApi = async (term = '') => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term,
+          limit: 50,
+          location: 'Hermosillo, Sonora',
+        },
+      });
 
-    const { data: { business } } = response;
-    setResults(business);
+      const { data: { business } } = response;
+      setResults(business);
+    } catch (error) {
+      console.error(error);
+      setErrorMsg('Something went wrong');
+    }
   };
+
+  useEffect(() => {
+    searchApi('Tacos');
+  }, []);
 
   return (
     <View>
-      <SearchBar onSubmit={onSearchSubmit} />
+      <SearchBar onSubmit={searchApi} />
       <Text>Search Screen</Text>
       <Text>{`Found ${results.length} results!`}</Text>
+      {
+        errorMsg ? <Text>{errorMsg}</Text>
+          : null
+      }
     </View>
   );
 };
